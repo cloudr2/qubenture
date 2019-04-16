dba64571d06ac478c83da5e1ebe8db3ccd9492ca
cloudr2 <alejandro.popovsky@gmail.com>
2019-04-16 01:22:03 -0300

    initial commit

---
+++ Assets/Scripts/Entities/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class Character : Entity
+{
+    [Header("Stats")]
+    protected float health;
+    protected float speed;
+
+    protected float currentHealth;
+
+    void Start() {
+        currentHealth = health;
+    }
+
+
+}
---
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character, IHiteable, IDestructible
+{
+    
+
+    public void Destroy() {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage) {
+        currentHealth -= damage;
+        if (currentHealth <= 0) {
+            Destroy();
+        }
+    }
+}
