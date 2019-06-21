8f0256480d92094d1e84cff0f918d466c225d6f2
AlePops <alejandro.popovsky@gmail.com>
2019-06-21 18:38:16 -0300

    added update manager script

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUpdateable
+{
+    void CustomUpdate();
+}
+
+public class UpdateManager : MonoBehaviour
+{
+    #region singleton
+    private static UpdateManager _instance;
+    public static UpdateManager Instance { get { return _instance; } }
+
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    private static List<IUpdateable> updateableObjects = new List<IUpdateable>();
+
+    public static void Register(IUpdateable obj)
+    {
+        if (obj == null)
+            return;
+
+        updateableObjects.Add(obj);
+    }
+
+    public static void Unregister(IUpdateable obj)
+    {
+        if (obj == null)
+            return;
+
+        updateableObjects.Remove(obj);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < updateableObjects.Count; i++)
+        {
+            updateableObjects[i].CustomUpdate();
+        }
+    }
+}
