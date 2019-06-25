2bea265fbfacf4ba8605afaa3743cb6f159da51b
AlePops <alejandro.popovsky@gmail.com>
2019-06-25 18:18:47 -0300

    added 3 levels and level progression flow

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour, IUpdateable
     private Enemy[] enemies = null;
     private Transform bossHolder;
     private bool bossSpawned = false;
+    private bool bossStage;
 
     #region Singleton
     void Awake() {
@@ -55,6 +56,13 @@ public class GameManager : MonoBehaviour, IUpdateable
         Time.timeScale = 0f;
         UIManager.instance.ShowResultScreen(true);
         UIManager.instance.SetResultText(state);
+        Invoke("NextLevel", 3);
+    }
+
+    public void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1, LoadSceneMode.Single);
     }
 
     public void LoadLevel(int sceneIndex)
@@ -71,12 +79,14 @@ public class GameManager : MonoBehaviour, IUpdateable
     {
         bossHolder = GameObject.Find("BossHolder").transform;
         enemies = FindObjectsOfType<Enemy>();
+        bossStage = FindObjectOfType<SpawnManager>()._bossCanSpawn;
         if (enemies.Length <= 0)
         {
-            if (!bossSpawned) {
+            if (!bossSpawned && bossStage) {
                 GameObject boss = Instantiate(bossPrefab, bossHolder.position, Quaternion.identity).gameObject;
                 boss.transform.parent = bossHolder.transform;
                 bossSpawned = true;
+                print("boss spawned");
             }
             else
                 EndGame("WIN");
